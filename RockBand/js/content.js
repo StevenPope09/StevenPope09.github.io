131022c4f641ff9479c36f1af5b51991b698a816
Steven Pope <SJPope@student.fullsail.edu>
2019-05-14 15:57:59 -0500

    updated html
    Updated html further and re0worked header nav and the footer. Also made small updates though files

--- RockBand/js/content.js
+++ RockBand/js/content.js
@@ -2,3 +2,37 @@
  * Use AJAX to load the JSON and manipulate the HTML
  * https://joshbloom.github.io/dws1/data/rockband.json
 */
+var xhr = new XMLHttpRequest();
+
+var tags = "";
+
+xhr.onload = function()
+{
+
+
+    if (xhr.status === 200)
+    {
+    //save json data
+        var data = JSON.parse(xhr.responseText);
+        console.log(data);
+
+
+
+        tags+= '<ul>';
+        for (var i = 0; i < data.news.length; i++)
+        {
+            tags+= '<li>';
+            tags+= '<img src="' + data.news[i].imageURL + '"/>';
+            tags+= '<h3>' + data.news[i].title + '</h3>';
+            tags+='<p>' + data.news[i].postDate+'</p>'
+        }
+        tags+= '</ul>';
+
+        var content = document.querySelector('#blog');
+        content.innerHTML = tags;
+    }
+};
+//call ajax
+xhr.open('GET','https://joshbloom.github.io/dws1/data/rockband.json',true);
+xhr.send(null);
+
