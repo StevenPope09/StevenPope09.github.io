fcb0ec92acacad117ffdb099284f2af6b421ba0f
Steven Pope <sjpope@student.fullsail.edu>
2019-05-30 14:37:42 -0500

    more updates to events

--- RockBand/js/events.js
+++ RockBand/js/events.js
@@ -7,24 +7,31 @@ function readMore(event){
     var parent = target.parentElement;
     if (target){
         var test = '';
+        test+='<div id="moreInfo">';
         test+='<p>'+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
             "tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie ac."+ '</p>';
         test+='<p><button class="readless">'+"Read Less"+'</button>';
+        test+='</div>';
         target.insertAdjacentHTML('afterend',test);
         parent.removeChild(parent.querySelector('#team button'));
-    }
 
+        var lessInfo = document.querySelectorAll('#team .readless');
+        console.log(lessInfo);
+        for (var i = 0; i < lessInfo.length; i++) {
+            lessInfo[i].addEventListener('click',readLess)
+        }
+
+    }
 }
 
 function readLess(event) {
     var target = event.target;
     var parent = target.parentElement;
-    if (target) {
+    var less = document.querySelectorAll('#team #moreInfo');
+    for (var i = 0; i < less.length; i++) {
         var test = '';
-        parent.removeChild(parent.querySelector('#team button'));
-        parent.removeChild(parent.querySelector('#team p'));
         test+='<p><button class="link">'+"Read More"+'</button>';
-        target.insertAdjacentHTML('beforerend',test);
+        less[i].innerHTML= test;
     }
 }
 
@@ -34,13 +41,7 @@ function teamInfo() {
         moreInfo[i].addEventListener('click', readMore);
     }
 }
-if () {
-    var lessInfo = document.querySelectorAll('#team .readless');
 
-    for (var i = 0; i < lessinfo.length; i++) {
-        lessInfo[i].addEventListener('click', readLess)
-    }
-}
 
 
 
