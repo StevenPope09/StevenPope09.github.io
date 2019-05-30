ba4c9bb792f189e291ce31a78a887d28517d6948
Steven Pope <sjpope@student.fullsail.edu>
2019-05-30 16:43:46 -0500

    updated about page event

--- RockBand/js/events.js
+++ RockBand/js/events.js
@@ -5,34 +5,21 @@
     function readMore(event) {
         var target = event.target;
         var parent = target.parentElement;
-        if (target) {
-            var addedInfo = '';
-            addedInfo += '<div id="moreInfo">';
-            addedInfo += '<p>' + "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
-                "tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie ac." + '</p>';
-            addedInfo += '<p><button class="readless">' + "Read Less" + '</button>';
-            addedInfo += '</div>';
+         if (!parent.querySelector('#moreInfo')) {
+             var addedInfo = '';
+             addedInfo += '<div id="moreInfo">';
+             addedInfo += '<p>' + "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                 "tempor incididunt ut labore et dolore magna aliqua. Lorem donec massa sapien faucibus et molestie ac." + '</p>';
+             addedInfo += '</div>';
 
-            target.insertAdjacentHTML('afterend', addedInfo);
-            
-            parent.removeChild(parent.querySelector('#team button'));
+             target.insertAdjacentHTML('beforebegin', addedInfo);
 
-            var lessInfo = document.querySelectorAll('#team .readless');
-            console.log(lessInfo);
-            for (var i = 0; i < lessInfo.length; i++) {
-                lessInfo[i].addEventListener('click', readLess)
-            }
-        }
-    }
-
-    function readLess() {
-        var less = document.querySelectorAll('#team #moreInfo');
-        for (var i = 0; i < less.length; i++) {
-            var test = '';
-            test += '<p><button class="link">' + "Read More" + '</button>';
-            less[i].innerHTML = test;
-            teamInfo();
-        }
+             target.innerHTML = "Read Less";
+         }
+         else{
+             parent.removeChild(parent.querySelector('#moreInfo'));
+             target.innerHTML="Read More";
+         }
     }
 
     function teamInfo() {
