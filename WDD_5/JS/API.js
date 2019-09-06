8937eb20153a88dd11cc820421513a5bbf2a6d39
Steven Pope <sjpope@student.fullsail.edu>
2019-09-06 13:49:11 -0500

    updated POC for code to work properly

---
+++ WDD_5/JS/API.js
@@ -0,0 +1,45 @@
+const apiUrlBase = 'https://pokeapi.co/api/v2/pokemon/'
+key = 1
+const completeUrl = `${apiUrlBase}${key}`
+
+//console.log(completeUrl);
+
+fetch(completeUrl)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+
+
+        } else {
+            throw response;
+        }
+    })
+
+    .then(data => {
+        
+
+         //console.log(data.results[0].url);
+        apiTest = document.querySelector('#apiTest')
+        // console.log(data.id);
+        // console.log(data.name);
+        // console.log(data.sprites.front_default);
+        // console.log(data.weight);
+        if (apiTest) {
+            let pokemonInfo = ''
+            pokemonInfo += '<div>'
+            pokemonInfo += '<h2>' + data.name + '</h2>'
+            pokemonInfo += '<p>' + data.id + '</p>'
+            pokemonInfo += '<img src="' + data.sprites.front_default + '"/>'
+            pokemonInfo += '<p>' + data.weight + '</p>'
+            pokemonInfo += '</div>'
+
+            apiTest.innerHTML = pokemonInfo;
+
+        //     key++
+        //     console.log(key);
+         }
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
