8f3b52f26c4ef27e85ef049b1c383fed3818f285
Steven Pope <sjpope@student.fullsail.edu>
2019-09-15 14:37:28 -0500

    updated layout of Pokemon component

--- WDD_5/classic_pokedex/src/components/Pokemon.js
+++ WDD_5/classic_pokedex/src/components/Pokemon.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+
 
 export default class Pokemon extends Component {
     render() {
 
         return (
             <div>
-                <h1><em>{this.props.poke.name}</em></h1>
-                <h1>{this.props.poke.id}</h1>
-                <h1>{this.props.poke.type1}, {this.props.poke.type2}</h1>
-                <img src={this.props.poke.img} alt=""/>
+                <Grid container spacing={4}>
+                    <Grid item xs={4}>
+                        <div>
+                            <h3>Pokemon Name</h3>
+                            <h1><em>{this.props.poke.name}</em></h1>
+                        </div>
+                        <div>
+                            <h3>Pokemon ID</h3>
+                            <h1>{this.props.poke.id}</h1>
+                        </div>
+                        <div>
+                            <h3>Type</h3>
+                            <h1>{this.props.poke.type}</h1>
+                        </div>
+                        <div>
+                            <h3>Picture</h3>
+                            <img src={this.props.poke.img} alt="" />
+                        </div>
+
+                    </Grid>
+                </Grid>
             </div>
         )
     }
