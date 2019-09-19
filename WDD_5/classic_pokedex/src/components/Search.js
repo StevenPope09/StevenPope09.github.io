b8904be74bb3f8c24468277256b4aff1fd709407
Steven Pope <sjpope@student.fullsail.edu>
2019-09-19 15:29:17 -0500

    updated search component

--- WDD_5/classic_pokedex/src/components/Search.js
+++ WDD_5/classic_pokedex/src/components/Search.js
@@ -1,56 +1,33 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { Navbar, NavDropdown, Form, Nav, FormControl, Button } from 'react-bootstrap';
+
 
 const Search = props => {
-    // console.log(props);
     return (
-        <form
-            style={styles.searchForm}
-            onSubmit={props.searchPoke}>
-            <div style={styles.searchBar}>
-                <div class="md-form mt-0">
-                    <input
-                        class= "form-control"
-                        value={props.search}
-                        onChange={props.onSearchChange}
-                        style={styles.input}
-                        placeholder="Search..." />
-                    <Button variant="contained" component="span" color="primary">
-                        Search
-                        </Button>
-                </div>
-
-            </div>
-
-        </form>
+        <Form inline style={styles.form}>
+            <FormControl style={styles.search} type="text" placeholder="Search" className="mr-sm-2" />
+            <Button style={styles.srchButton} variant="outline-success">Search</Button>
+        </Form>
     )
 }
 
-export default Search
+export default Search;
 
 const styles = {
-    searchForm: {
-        marginTop: '1%',
-        marginBottom: '5%',
-        color: 'rgb(163,173,194)',
-        border: '5px soild black'
-
+    search: {
+        width: '55%'
     },
-    // searchBar: {
-    //     display: 'flex',
-    //    justifyContent: 'right'
-    // },
-    input: {
-        height: '35px',
-        width: '40%',
-        marginLeft: '21%',
-        marginRight: '1%',
-        fontSize: '1.0em',
-        borderRadius: '15px',
-        border: '.5px solid black',
-        padding: '8px'
-
+    srchButton: {
+        marginTop: '2%',
+        alignText: 'center',
+        backgroundColor: '#FF0000',
+        color: 'white',
+        border: '1.5px solid black'
     },
-
-
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '7%',
+        marginBottom: '3%'
+    }
 }
