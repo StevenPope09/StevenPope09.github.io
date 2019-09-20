36f54687274b950878f8d7b774873ca83abf6a5e
Steven Pope <sjpope@student.fullsail.edu>
2019-09-20 14:55:38 -0500

    updated code in Header for styling purposes

--- WDD_5/classic_pokedex/src/components/Header.js
+++ WDD_5/classic_pokedex/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Logo from '../images/logo.png'
 import { NavLink } from 'react-router-dom'
 //import { makeStyles } from '@material-ui/core/styles';
-import { Navbar, NavDropdown, Form, Nav, FormControl, Button, Container } from 'react-bootstrap';
+import { Navbar,Nav, Image} from 'react-bootstrap';
 
 
 //You can use appBar here from Material UI
@@ -11,23 +11,23 @@ const Header = props => {
     //const classes = useStyles();
     return (
         <Navbar style={styles.navBar} expand="lg">
-            <Navbar.Brand href="/"> <img style={styles.logo} src={Logo} /></Navbar.Brand>
-            <Container>
-                <Navbar.Brand href="/" style={styles.title}>Pokedex Collection</Navbar.Brand>
+            <Navbar.Brand href="/"> <Image style={styles.logo} src={Logo} fluid /></Navbar.Brand>
+            {/* <Container> */}
+                <Navbar.Brand href="/" style={styles.title}>Personal Pokedex</Navbar.Brand>
             
 
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <div>
-                        <Nav.Link style={styles.links} href="/">Pokedex</Nav.Link>
+                        <Nav.Link style={styles.links} href="/pokedex">Pokedex</Nav.Link>
                         <Nav.Link style={styles.links} href="/collection">Collections</Nav.Link>
                     </div>
 
                 </Nav>
 
             </Navbar.Collapse>
-            </Container>
+            {/* </Container> */}
         </Navbar>
     )
 }
@@ -37,18 +37,23 @@ let styles = {
     logo: {
         height: '100px',
         width: '100px',
-        marginRight: '3%'
+        //marginRight: '3%'
     },
     navBar: {
         backgroundColor: '#203FED',
         color: 'white',
         display: 'flex',
+        flexDirection: 'column',
+        width: '100%'
+        
 
     },
     links: {
+        
         color: 'white',
         //display: 'inline',
-        fontSize: '25px'
+        fontSize: '25px',
+        float: 'right'
     },
     title: {
         color: 'white',
