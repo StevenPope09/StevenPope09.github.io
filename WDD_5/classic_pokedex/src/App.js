90607b3ed63edda13ceb177af22408366e16b39f
Steven Pope <sjpope@student.fullsail.edu>
2019-09-20 14:54:25 -0500

    chaged home page in route and added home.js to file

--- WDD_5/classic_pokedex/src/App.js
+++ WDD_5/classic_pokedex/src/App.js
@@ -6,7 +6,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Pokedex from './components/pages/Pokedex';
 import Collection from './components/pages/Collection';
-
+import Home from './components/pages/Home';
+import { Container } from 'react-bootstrap';
 
 function App() {
 
@@ -20,9 +21,10 @@ function App() {
     <Router>
       <div className="App">
         <Header pgTitle="Classic Pokedex" />
-        <Route path="/" exact component={Pokedex} />
+        <Route path="/" exact component={Home} />
+        <Route path="/pokedex" exact component={Pokedex} />
         <Route path="/collection/" component={Collection} />
-        
+        <Footer />
 
       </div>
     </Router>
