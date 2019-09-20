d95ca5a24b55c94b20cbadf91a1d8e60911f76f7
Steven Pope <sjpope@student.fullsail.edu>
2019-09-20 14:55:12 -0500

    updated footer component and added it to App.js

--- WDD_5/classic_pokedex/src/components/Footer.js
+++ WDD_5/classic_pokedex/src/components/Footer.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Container } from 'react-bootstrap';
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+const Footer = () => {
+    return (
+        <footer style={styles.footer}>
+            <Container>
+                <h4>Steven Pope Design & Development 2019 All Rights reserved.  </h4>
+                <p>Email: <a style={styles.anchor} href="mailto: sjpope@student.fullsail.edu">Sjpope@student.fullsail.edu</a></p>
+                <p>Phone Number: (210) 216-4119</p>
+                <p>Address: 1234 Somewhere Street, Some Place NA 12345</p>
+            </Container>
+
+        </footer>
+    )
+}
+export default Footer
+
+const styles = {
+    footer: {
+        border: '1px solid black',
+        backgroundColor: '#203FED',
+        color: 'white',
+        marginTop: '3%',
+        paddingTop: '15px'
+    },
+    anchor: {
+        color: 'white'
     }
 }
-
